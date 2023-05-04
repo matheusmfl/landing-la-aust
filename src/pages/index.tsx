cd668a00959f25a6cfaecd2281a07c39e48c4f84
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-04 18:36:05 -0300

    Create metaDatas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,28 @@ import Section7 from '@/components/Section7'
 import Section8 from '@/components/Section8'
 import Section9 from '@/components/Section9'
 import Section12 from '@/components/section12'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title> LaAust Landing</title>
+        <meta
+          property="og:description"
+          content="Desenvolvimento de soluções para você e sua empresa"
+        />
+        <meta
+          name="description"
+          content="Soluções digitais para você e sua empresa"
+        />
+        <meta
+          name="keywords"
+          content="branding, logo, web designer, landing pages, sites, criação de marcas"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" />
+      </Head>
       <Header />
       <Section1 />
       <Section2 />
