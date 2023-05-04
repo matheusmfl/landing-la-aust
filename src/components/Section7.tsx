8daaed30258251f491c1d3c7bbf5884fcbed7931
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-04 15:17:49 -0300

    Create section feedback

---
+++ src/components/Section7.tsx
@@ -0,0 +1,57 @@
+import { H2 } from './microComponents/H2'
+import image1 from '../../public/sectionFeedback/FEEDBACK1.png'
+import image2 from '../../public/sectionFeedback/FEEDBACK2.png'
+import image3 from '../../public/sectionFeedback/FEEDBACK3.png'
+import image4 from '../../public/sectionFeedback/FEEDBACK4.png'
+import image5 from '../../public/sectionFeedback/FEEDBACK7.png'
+import image6 from '../../public/sectionFeedback/FEEDBACK9.png'
+import Image from 'next/image'
+
+export default function Section7() {
+  return (
+    <section
+      className="py-10 px-5 text-center flex flex-col gap-10 bg-cover bg-opacity-80 bg-center"
+      style={{ backgroundImage: "url('/feedbackBg.png')" }}
+    >
+      <H2 align="white">O que dizem nossos clientes</H2>
+      <div className="flex flex-col gap-5 items-center">
+        <div>
+          <Image
+            src={image1}
+            alt="Balão de fala relatando como foi trabalhar com a LaAust"
+          />
+        </div>
+        <div>
+          <Image
+            src={image2}
+            alt="Balão de fala relatando como foi trabalhar com a LaAust"
+          />
+        </div>
+        <div>
+          <Image
+            src={image3}
+            alt="Balão de fala relatando como foi trabalhar com a LaAust"
+          />
+        </div>
+        <div>
+          <Image
+            src={image4}
+            alt="Balão de fala relatando como foi trabalhar com a LaAust"
+          />
+        </div>
+        <div>
+          <Image
+            src={image5}
+            alt="Balão de fala relatando como foi trabalhar com a LaAust"
+          />
+        </div>
+        <div>
+          <Image
+            src={image6}
+            alt="Balão de fala relatando como foi trabalhar com a LaAust"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Section3 from '@/components/Section3'
 import Section4 from '@/components/Section4'
 import Section5 from '@/components/Section5'
 import { Section6 } from '@/components/Section6'
+import Section7 from '@/components/Section7'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Section4 />
       <Section5 />
       <Section6 />
+      <Section7 />
     </>
   )
 }
