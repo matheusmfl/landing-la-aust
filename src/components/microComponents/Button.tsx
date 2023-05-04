6af493aacca1817a8fa43e43782a5b4d492caabd
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-04 14:26:38 -0300

    Config text on button

--- src/components/microComponents/Button.tsx
+++ src/components/microComponents/Button.tsx
@@ -5,7 +5,7 @@ type buttonProps = {
 }
 export function Button({ children }: buttonProps) {
   return (
-    <button className="py-3 px-5 bg-zinc-950 rounded-[4px] font-Montserrat flex items-center justify-center">
+    <button className="py-3 px-5 bg-zinc-950 text-white rounded-[4px] font-Montserrat flex items-center justify-center">
       {children}
     </button>
   )
